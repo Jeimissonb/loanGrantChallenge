4d3458937125bc4ed1cbded85a8a44d000e53a7b
Fernando <fernando-danatas@hotmail.com>
2022-07-07 01:17:39 -0300

    validations done!

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Mail\newLaravelTips;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Redirect;
@@ -13,13 +14,31 @@ use stdClass;
 class LoginController extends Controller
 
 {
-    protected $emailFixed = 'fernando-danatas@hotmail.com' || '';
-
     protected $randomNumber;
-
 
     public function SingIn(Request $request)
     {
+
+        $request->validate(
+            [
+                'email' => 'required|email',
+
+                //outro tipo de validação, pois no html existem validações, essas são para ter uma segunda opção
+                'cpf1'  => 'required|digits:1',
+                'cpf2'  => 'required|digits:1',
+                'cpf3'  => 'required|digits:1',
+                'cpf4'  => 'required|digits:1',
+                'email_code' => 'required|digits:6'
+            ],
+            [
+                'email.required' => 'Email possui preenchimento obrigatório!',
+                'cpf1.required'  => '1º digito do CPF possui preenchimento obrigatório!',
+                'cpf2.required'  => '2º digito do CPF possui preenchimento obrigatório!',
+                'cpf3.required'  => '3º digito do CPF possui preenchimento obrigatório!',
+                'cpf4.required'  => '4º digito do CPF possui preenchimento obrigatório!',
+                'email_code.required' => 'Código enviado ao email possui preenchimento obrigatório!'
+            ]
+        );
 
         $user = User::whereRaw('SUBSTRING(cpf, 1, 1) = ' . $request->cpf1)
             ->whereRaw('SUBSTRING(cpf, 2, 1) = ' . $request->cpf2)
@@ -33,7 +52,11 @@ class LoginController extends Controller
             Auth::login($user); //efetuando a operação de autenticação
             return Redirect('dados');
         } else {
-            echo ('Dados incorretos');
+            // retorna com redirect para a última rota, com Inputs do usuário e Mensagem de Erro
+            return redirect()
+                ->back()
+                ->withInput()
+                ->withErrors(['Credenciais não encontradas em nossas bases de dados :(']);
         }
     }
 
@@ -46,22 +69,32 @@ class LoginController extends Controller
 
         Auth::logout();
 
-        return Redirect::back()->with('message', 'Operation Successful !');;
+        return Redirect::back()->with('message', 'Operation Successful !');
     }
 
     public function SendMail(Request $request)
     {
         $emailValue = $request->input('email');
 
+        $request->validate(
+            [
+                'email' => 'required|email',
+            ],
+            [
+                'email.required' => 'Preencha o campo email para receber o código!',
+            ]
+        );
+
         //dando um select na tabela de users buscando pelo email mockado
         $verifyEmailExists = User::where('email', $emailValue)->get();
 
         if (!$verifyEmailExists || count($verifyEmailExists) === 0) {
-            echo "<script>
-            alert('Email não encontrado no banco de dados!')
-            </script>";
-            return view('welcome');
+            return redirect()
+                ->back()
+                ->withInput()
+                ->withErrors(['Email não encontrado em nossas bases de dados :(']);
         }
+
         $emailValidate = '';
         foreach ($verifyEmailExists as $e) {
             $emailValidate = $e['email'];
@@ -83,11 +116,14 @@ class LoginController extends Controller
 
         $this->randomNumber = $numbers;
         //passando o email fixo.
+
         Mail::send(new newLaravelTips($userMail));
         User::where('email', $emailValidate)->update(['email_code' => $userMail->numbers]);
-        echo "<script>
-            alert('Email Enviado com sucesso!')
-            </script>";
-        return view('welcome');
+
+
+
+        return redirect()
+            ->back()
+            ->with('success', 'Código de confirmação enviado para: ' . '<strong>' . $emailValue . '</strong>');
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,63 +1,92 @@
-<?php include('./includes/head.php'); ?>
+<?php include './includes/head.php'; ?>
 
-  <main class="page" id="emprestimo">
+<main class="page" id="emprestimo">
     <div class="container">
-      <div class="row">
-        <div class="col-md-10 col-lg-7 col-xl-6">
-          <div class="box">
-            <h1 class="title">Informe seus dados</h1>
+        <div class="row">
+            <div class="col-md-10 col-lg-7 col-xl-6">
+                <div class="box">
+                    <h1 class="title">Informe seus dados</h1>
 
-            <form method="POST" action="{{ route('LoginController.SingIn') }}" class="form">
-                @csrf
-              <div class="form-row">
-                <div class="col-12 form-placeholder">
-                  <label for="email"  class="form-label">Digite o seu e-mail do trabalho:</label>
-                  <input type="email" name="email" id="email" class="form-control" required>
-                </div>
-              </div>
+                    <form method="POST" action="{{ route('LoginController.SingIn') }}" class="form">
+                        @csrf
+                        <div class="form-row">
+                            <div class="col-12 form-placeholder">
+                                <label for="email" class="form-label">Digite o seu e-mail do trabalho:</label>
+                                <input type="email" name="email" id="email" class="form-control">
+                            </div>
+                            <div>
+                                <button type="submit" class="btn btn-info w-100"
+                                    formaction="{{ route('LoginController.SendMail') }}">Enviar
+                                </button>
+                            </div>
+                        </div>
 
-              <div class="form-row">
-                <div class="col-md-6">
-                  <label for="cpf-1" class="form-label">Digite os 4 primeiros dígitos do seu CPF:</label>
-                </div>
-                <div class="col-2 col-md">
-                    <input type="text" name="cpf1" id="cpf1" class="form-control" inputmode="numeric" minlength="1" maxlength="1" size="1" required>
-                </div>
-                <div class="col-2 col-md">
-                    <input type="text" name="cpf2" id="cpf2" class="form-control" inputmode="numeric" minlength="1" maxlength="1" size="1" required>
-                </div>
-                <div class="col-2 col-md">
-                    <input type="text" name="cpf3" id="cpf3" class="form-control" inputmode="numeric" minlength="1" maxlength="1" size="1" required>
-                </div>
-                <div class="col-2 col-md">
-                  <input type="text" name="cpf4" id="cpf4" class="form-control" inputmode="numeric" minlength="1" maxlength="1" size="1" required>
-                </div>
-              </div>
+                        <div class="form-row">
+                            <div class="col-md-6">
+                                <label for="cpf-1" class="form-label">Digite os 4 primeiros dígitos do seu
+                                    CPF:</label>
+                            </div>
+                            <div class="col-2 col-md">
+                                <input type="text" name="cpf1" id="cpf1" class="form-control"
+                                    inputmode="numeric" minlength="1" maxlength="1" size="1">
+                            </div>
+                            <div class="col-2 col-md">
+                                <input type="text" name="cpf2" id="cpf2" class="form-control"
+                                    inputmode="numeric" minlength="1" maxlength="1" size="1">
+                            </div>
+                            <div class="col-2 col-md">
+                                <input type="text" name="cpf3" id="cpf3" class="form-control"
+                                    inputmode="numeric" minlength="1" maxlength="1" size="1">
+                            </div>
+                            <div class="col-2 col-md">
+                                <input type="text" name="cpf4" id="cpf4" class="form-control"
+                                    inputmode="numeric" minlength="1" maxlength="1" size="1">
+                            </div>
+                        </div>
 
-              <div class="form-row">
-                <div class="col-md-6">
-                  <label for="email_code" class="form-label">Digite o código recebido pelo e-mail:</label>
-                </div>
-                <div class="col-md-6">
-                  <input type="text" inputmode="numeric" minlength="6" maxlength="6" size="6" required name="email_code" id="email_code" class="form-control">
-                </div>
-                <div class="col-12 form-info">*Código enviado para o e-mail preenchido no 1º campo</div>
-              </div>
+                        <div class="form-row">
+                            <div class="col-md-6">
+                                <label for="email_code" class="form-label">Digite o código recebido pelo e-mail:</label>
+                            </div>
+                            <div class="col-md-6">
+                                <input type="text" inputmode="numeric" minlength="6" maxlength="6" size="6"
+                                    name="email_code" id="email_code" class="form-control">
+                            </div>
+                            <div class="col-12 form-info">*Código enviado para o e-mail preenchido no 1º campo</div>
+                        </div>
 
-              <div class="form-row form-btn">
-                <div class="col-12">
-                  <button type="submit" class="btn btn-green">Continuar</button>
-                  <button type="submit"  class="btn btn-green" formaction="{{ route('LoginController.SendMail') }}">Enviar
-                </button>
+                        <div class="form-row form-btn">
+                            <div class="col-12">
+                                <button type="submit" class="btn btn-green">Continuar</button>
 
+
+                            </div>
+                            <div class="form-row form-btn">
+                            </div>
+                    </form>
+                    <div class="max-width mt-5">
+                        <!--FORM VALIDATE-->
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        @if (\Session::has('success'))
+                            <div class="alert alert-success">
+                                <ul>
+                                    <li>{!! \Session::get('success') !!}</li>
+                                </ul>
+                            </div>
+                        @endif
+                    </div>
                 </div>
-                <div class="form-row form-btn">
-              </div>
-            </form>
-          </div>
+            </div>
         </div>
-      </div>
     </div>
-  </main>
+</main>
 
-<?php include('includes/footer.php'); ?>
+<?php include 'includes/footer.php'; ?>
